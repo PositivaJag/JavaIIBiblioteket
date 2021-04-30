2f78e2e4560036c3bf948b6a50bb0f9ff8b4b46e
PositivaJag <65033208+PositivaJag@users.noreply.github.com>
2021-04-30 11:22:31 +0200

    Now includes Calculation of celcius to farhenheit. App, primary and seconadry controll removed.

---
+++ src/main/java/org/biblioteket/LoginController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.biblioteket;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author Jenni
+ */
+public class LoginController {
+    
+}
