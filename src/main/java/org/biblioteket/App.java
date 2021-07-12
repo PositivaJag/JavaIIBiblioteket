6974a9c9e5b1852f7fbfeaa0bc5d2124a0aa2acf
Wasabichicken <ericj+github@acc.umu.se>
2021-07-12 12:00:24 +0200

    Sätt en stage title. Det hjälper min fönsterhanterare. (#1)
    * Sätt en stage title. Det hjälper min fönsterhanterare.
    
    * Byter fönstertitel.
    
    Co-authored-by: Eric Jönsson <ericj@acc.umu.se>

--- src/main/java/org/biblioteket/App.java
+++ src/main/java/org/biblioteket/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
       
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Biblioteket");
         stage.show();       
     }
 
